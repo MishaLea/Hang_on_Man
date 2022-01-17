e6dbba2749ee5e02c346c85977f0a78b62937a27
MishaLea <mishafreestone@ymail.com>
2022-01-17 16:39:16 +0000

    tidy whitespace and extra lines

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import random words for generating
 random word choice for user.
 """
 import random
-from words import words   # takes words from words.py
 import string  # ???
+from words import words   # takes words from words.py
 
 
 def get_valid_word(words):  # define function and pass it a list of words
-    word = random.choice(words)   # variable 'word' w/ assigned value 
+    word = random.choice(words)  # variable 'word' w/ assigned value.
     while '-' in word or ' ' in word:
-        word = random.choice(words)  # keep iterating until not True. 
+        word = random.choice(words)  # keep iterating until not True.
 
     return word
 
@@ -22,15 +22,16 @@ def hangman():   # record letters correctly guessed and validity of answer/s.
     used_letters = set()  # what user has guessed
 
 
-    # User input 
+# User input
     user_letter = input("Guess a letter or word: ").upper()
     if user_letter in alphabet - used_letters:
         used_letters.add(user_letter)
         if user_letter in word_letters:
             word_letters.remove(user_letter)
 
-    
-    # if letter just enetered in used_letters, invalid as already used. 
+
+
+# if letter just enetered in used_letters, invalid as already used. 
     elif user_letter in used_letters:
         print("You already used that character, guess again.")
 
