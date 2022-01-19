c2aab12480fa1bb5d070e1796402e5a3eb03efeb
MishaLea <mishafreestone@ymail.com>
2022-01-19 08:41:07 +0000

    comments for each stage of game

--- run.py
+++ run.py
@@ -1,61 +1,15 @@
-"""
-import random words for generating
-random word choice for user.
-"""
-import random
-import string  # ???
-from words import words   # takes words from words.py
+# set up the word and hidden list.
 
+# loop until either the player has won or lost.
 
-def get_valid_word(words):  # define function and pass it a list of words
-    word = random.choice(words)  # variable 'word' w/ assigned value.
-    while '-' in word or ' ' in word:
-        word = random.choice(words)  # keep iterating until not True.
+    # dislay the current boards, guessed letters and attempts remaining.
 
-    return word.upper()
+    # ask the player for a character.
 
+    # if the player guessed correct, show all matched letters and print message.
 
-def hangman():   # record letters correctly guessed and validity of answer/s.
-    word = get_valid_word(words)
-    word_letters = set(word)  # variable to save all letters in a word as a set
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what user has guessed
+    # if player guessed wrong, print failure message and incorrect attempts.
 
-    lives = 7
+    # if the player wins print congrats message and stop the looping.
 
-
-# User input
-    while len(word_letters) > 0 and lives > 0:    # letters used
-        # ".join" turns list into interval/string. ("a", "b", "c" --> "a b c")
-        print('You have', lives, 'lives left and used these letters:', ' '.join(used_letters))
-
-        # current word ( ie. W - R D)
-        word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('Current word: ', ' '.join(word_list))
-
-        user_letter = input('Guess a letter or word: ').upper()
-        if user_letter in alphabet - used_letters:
-            used_letters.add(user_letter)
-            if user_letter in word_letters:
-                word_letters.remove(user_letter)
-                print(' ')
-
-            else:
-                lives = lives - 1  # takes life if wrong
-                print('\nYour letter,', user_letter, 'is not in the word.')
-# if letter just enetered in used_letters, invalid as already used. 
-        elif user_letter in used_letters:
-            print('You already used that character, guess again.')
-
-        else:
-            print('Invalid choice - try again using only letters.')
-    
-    # iterates while length of word is greater than zero***
-    if lives == 0:
-        print('You died, sorry. The word was', word)
-    else: 
-        print('YAY! You guessed the word', word,'!!')
-
-
-if __name__ == '__main__':
-    hangman()
+    # if player has lost, print fail message and stop loop. 
