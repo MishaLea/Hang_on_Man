6f1b2749c90be30a3a596699fbd7df49f55cffd4
MishaLea <mishafreestone@ymail.com>
2022-01-20 11:29:09 +0000

    add words list and random import

--- run.py
+++ run.py
@@ -1,22 +1,30 @@
+import random
+
 # set up the word and hidden list.
-word = list("apple")
+words = ['experiment', 'leash', 'me', 'second', 'rehearsal', 'carry',
+        'shine', 'great', 'motif', 'intervention', 'shake', 'narrow',
+        'bridge', 'story', 'blonde', 'field', 'man', 'rotate', 'disappoint',
+        'purpose', 'irony', 'consideration', 'weapon', 'cable', 'governor',
+        'enlarge', 'aspect', 'free', 'laser', 'pill']
 hidden = []
-for character in word:
+
+word = random.choice(words)
+
+for character in words:
     hidden.append("_")
 
 attempts = 0
-max_attempts = 4
+max_attempts = 5
 
 # loop until either the player has won or lost.
 isGameOver = False
 while not isGameOver:
 
     # dislay the current boards, guessed letters and attempts remaining.
-    print(f'You have {attempts} attempts ramaining')
+    print(f'You have {max_attempts - attempts} attempts ramaining')
 
-    hiddenString = ' '.join(hidden)
-    print(f'The current word is: {hiddenString}')
-    
+    print(f"The current word is: {' '.join(hidden)}")
+
     print("     ------")
     print("     |    |")
     print("     |    " + ("O" if attempts > 0 else ""))
@@ -25,24 +33,30 @@ while not isGameOver:
     print("     |    " + ("/\\" if attempts > 3 else ""))
     print(" --------")
 
-
     # ask the player for a character.
     letterGuessed = input("Please guess a letter: ")
+    print("\n\n\n\n")
 
-    if letterGuessed in word:
-        for i in range(len  (word)  ):
-            character = word[i]
+    if letterGuessed in words:
+        # if guessed correct, show all matched letters and print message
+        print(f'Nice work, the letter {letterGuessed} is in the word!')
+        for i in range(len(word)):
+            character = words[i]
             if character == letterGuessed:
-                hidden[i] = word[i]
-                word[i] = '_'
-
-    print(hidden)
-    print(word)
-
-    # if the player guessed correct, show all matched letters and print message
-
-    # if player guessed wrong, print failure message and incorrect attempts.
+                hidden[i] = words[i]
+                words[i] = '_'
+    else:
+        # if guessed wrong, print failure message and incorrect attempts.
+        print(f'Unlucky, the letter {letterGuessed} is not in the word.')
+        attempts += 1
 
     # if the player wins print congrats message and stop the looping.
+    if (all('_' == char for char in word)):
+        print("Congrats, Man! You won!")
+        isGameOver = True
 
-    # if player has lost, print fail message and stop loop. 
+    # if player has lost, print fail message and stop loop.
+    if (attempts == max_attempts):
+
+        print("Ouch, better luck next time!")
+        isGameOver = True
